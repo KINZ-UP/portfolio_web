43469a3e39bb56e209f63d49d3a8eb23ff869b0a
kinz-up <berksmile@gmail.com>
2021-04-18 14:41:18 +0900

    feat: implement sample post page

--- src/Routes.js
+++ src/Routes.js
@@ -4,8 +4,9 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Blog from "./pages/Blog";
 import Portfolio from "./pages/Portfolio";
+import Blog from "./pages/Blog";
+import Post from "./pages/Post";
 
 const Routes = () => {
   return (
@@ -15,7 +16,8 @@ const Routes = () => {
         <Route exact path={"/"} component={Home} />
         <Route exact path={"/about"} component={About} />
         <Route path="/portfolio" component={Portfolio} />
-        <Route path="/blog" component={Blog} />
+        <Route exact path="/blog" component={Blog} />
+        <Route path="/blog/:id" component={Post} />
       </Switch>
     </BrowserRouter>
   );
--- src/style/global.js
+++ src/style/global.js
@@ -1,6 +1,12 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+
+  @font-face {
+    font-family: "SsangmunDong";
+    src: url("./font/Typo_SsangmunDongB.ttf");
+  }
+
   html {
     @media (max-width: 1000px) {
       font-size: 80%;
