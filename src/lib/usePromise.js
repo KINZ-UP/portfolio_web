c10605fcb5f61e1700f6bb809849d1b15e3b9425
kinz-up <berksmile@gmail.com>
2021-04-24 20:04:18 +0900

    feat: implement usePromise

---
+++ src/lib/usePromise.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+export default function usePromise(promiseCreater, deps) {
+  const [loading, setLoading] = useState(true);
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    async function request() {
+      try {
+        const resp = await promiseCreater();
+        setResponse(resp.data);
+      } catch (e) {
+        setError(true);
+      }
+      setLoading(false);
+    }
+    request();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, deps);
+
+  return [loading, response, error];
+}
