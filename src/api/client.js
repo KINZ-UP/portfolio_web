b489a2507648bf19989aba2bc92624e9e936f2c2
kinz-up <berksmile@gmail.com>
2021-04-27 17:38:02 +0900

    feat: modify server url

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const client = axios.create();
-client.defaults.baseURL = "http://localhost:8000";
+client.defaults.baseURL =
+  "http://ec2-13-125-136-174.ap-northeast-2.compute.amazonaws.com:8000";
 
 export default client;
