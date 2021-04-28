f28ae91c3778c66cecffb1237185bc24e5de376b
kinz-up <berksmile@gmail.com>
2021-04-28 21:05:58 +0900

    feat: set api uri

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
 const client = axios.create();
 client.defaults.baseURL =
-  "http://ec2-13-125-136-174.ap-northeast-2.compute.amazonaws.com:8000";
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8000'
+    : 'http://ec2-13-125-136-174.ap-northeast-2.compute.amazonaws.com:8000';
 
 export default client;
